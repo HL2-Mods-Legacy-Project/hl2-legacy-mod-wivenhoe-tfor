acf52b292d75a69df490e1c798fc8c4d049a3198
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:06:01 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Wivenhoe_TFOR" `
+  -ModName "Wivenhoe: The Fall of Ravenholm" `
+  -ModFolder "TheFallOfRavenholm" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/wivenhoe-tfor/TheFallOfRavenholm-Original.zip" `
+  -ModBaseFilesUrlHash "b910661c50ea14524823c6a243af14308a22d0ed2b49b71dce88cef798c1218d" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
